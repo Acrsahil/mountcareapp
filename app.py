bafd8e8dda9e661ac6dc5aea924f2cd1cd205832
Acrsahil <acharyasahil2003@gmail.com>
2024-10-14 22:17:21 +0545

    file name change

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+#Create a form class
 
 # Create a route decorator
 @app.route('/')
 def index():
     first_name = "Binod Khadka"
-    stuff = "This is strong Text"
-
-
+    stuff = "I love Arch Linux"
     favorite_pizza =["Pepperoni","Cheese","Mushrooms",'Onions',"Pineapple",20]
     return render_template('index.html',first_name=first_name,stuff=stuff,favorite_pizza=favorite_pizza)
 
@@ -24,6 +28,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"),500
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
